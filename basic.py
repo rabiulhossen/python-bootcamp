e6bc47a873c8d54b77b7b95e05194a336173a8e1
Rabiul Hossen <rhsimon69@gmail.com>
2023-12-21 21:44:15 +0600

    basic about python

---
+++ basic.py
@@ -0,0 +1,20 @@
+# python is dynamic language that means it can reassign value
+
+'''"""  """
+variable names best pracatise pep8
+
+type() function check data types
+\n used for new line
+len() for length of string or list
+index means serial
+
+slicing means grab a subsection of multiple characteres.\
+    slice has following stucture : [start: stop: step]
+
+
+'''
+print(" hello \n rabiul")
+string = "ffdfdfad"
+print(string[-3]) #index of charactar  -(minus) used for reverse index like -3 means last 3
+
+print("slicing_Example"[3:6]) #3:6 means index start from 3 and end 6
