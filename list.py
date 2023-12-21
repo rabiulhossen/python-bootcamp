c67a94584a856f3d726ce0ba2f62affbe1fffdcb
Rabiul Hossen <rhsimon69@gmail.com>
2023-12-21 21:09:31 +0600

    initial commit done[list]

---
+++ list.py
@@ -0,0 +1,20 @@
+list = [3,4,5,6,78,4,46]
+print(list[:-1])
+
+index =0
+for index in range(len(list)):
+   print(list[index])
+  # index = index+ list[index]
+  
+# input from user
+
+talika = []
+
+n = int(input("enter your number: "))
+
+for i in range(n):
+    new = input()
+    talika.append(new) #new item added
+   
+    
+print(talika)
