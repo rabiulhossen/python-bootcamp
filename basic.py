5b2a51ea0442ae63e1892ab1a9b8f2e40ae6bf28
Rabiul Hossen <rhsimon69@gmail.com>
2023-12-22 21:02:59 +0600

    operator

--- basic.py
+++ basic.py
@@ -2,19 +2,39 @@
 
 '''"""  """
 variable names best pracatise pep8
-
+strings aren't mutable(that means can't use indexing to change individual elements of a string)
 type() function check data types
 \n used for new line
 len() for length of string or list
 index means serial
 
-slicing means grab a subsection of multiple characteres.\
-    slice has following stucture : [start: stop: step]
-
+slicing means grab a subsection of multiple characteres.
+slice has following stucture : [start: stop: step]
+string formatting:
+1. format() method
+2. f-strings (f)
 
 '''
+a = 444
+b = a
+c = b
+
+
 print(" hello \n rabiul")
 string = "ffdfdfad"
 print(string[-3]) #index of charactar  -(minus) used for reverse index like -3 means last 3
 
 print("slicing_Example"[3:6]) #3:6 means index start from 3 and end 6
+
+#format
+print("this is string{} ".format(":insert something"))
+
+# serial maintain
+print(" The {} {} {} {}".format("brown", "quick", "jump", "fox"))
+# in {} u can index here
+print(" The {1} {0} {3} {2}".format("brown", "quick", "jump", "fox"))
+
+#f strings
+name ="rabiul"
+stack ="MERN"
+print(f"my name is {name} and stack is {stack}")
---
+++ operator.py
@@ -0,0 +1,44 @@
+'''a = int(input("enter a nukmber: "))
+b = int(input("enter a nukmber: "))
+
+avg = (a+b)/2
+print(f"average is {avg}")
+
+
+
+def average(a,b):
+    avg= (a+b)/2
+    return avg
+   
+    
+print(average(5,7))
+
+#bit-wise operator
+
+x =4
+y =5
+print(x|y)
+print(y>>y)
+
+
+'''
+
+
+centi = int(input("enter your centigrade temperature: "))
+
+fahrenheit = float(((9*centi)/5)+32)
+
+print(f" fahrenheit temperature is {fahrenheit}")
+
+
+radius = int(input("radius is: "))
+
+pie = 3.1416
+
+circumference = 2* pie * radius
+
+print(f" circumference is {circumference}")
+
+area = pie * radius**2
+
+print(f"area is {area}")
