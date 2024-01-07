a6b3dbe5731bba7cc4a86c67bf63fef4166b1727
Rabiul Hossen <rhsimon69@gmail.com>
2024-01-08 01:13:21 +0600

    file created

--- file.py
+++ file.py
@@ -1,4 +1,13 @@
-file = open("file1.txt", "w")
-file.write("hello everyone")
+file = open("file1.txt", "w") #create file
+lines = ["hello world", "welcome to python", "enjoy learning python"] #content in file
+file.writelines(lines) #print the lines
 file.close()
 print("data written into the file")
+
+
+fout = open('output.txt', 'w')
+line1 = "This here's the wattle,\n"
+fout.write(line1)
+x = 52
+fout.write(str(x))
+
