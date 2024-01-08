6adff25dfd34cdea13c9bbef20cfad13417b16bb
Rabiul Hossen <rhsimon69@gmail.com>
2024-01-08 12:27:52 +0600

    file operation

--- file.py
+++ file.py
@@ -1,13 +1,28 @@
-file = open("file1.txt", "w") #create file
-lines = ["hello world", "welcome to python", "enjoy learning python"] #content in file
-file.writelines(lines) #print the lines
+"""  
+file = open("file1.txt", "w")  # create file
+lines = ["hello world", "welcome to python", "enjoy learning python"]  # content in file
+file.writelines(lines)  # print the lines
+print(list(file))
 file.close()
 print("data written into the file")
 
 
-fout = open('output.txt', 'w')
-line1 = "This here's the wattle,\n"
-fout.write(line1)
-x = 52
-fout.write(str(x))
 
+file2 = open("file2.txt", "r")
+print(file.read(100))
+
+
+print("first line:", file.readline())
+print("second line:", file.readline())
+file.close()
+"""
+
+file = open("file3.txt", "w")
+
+#rename old filename
+import os
+os.rename("file2.txt", "changeName.txt")
+print("file renamed")
+
+#remove file
+os.remove("file3.txt")
